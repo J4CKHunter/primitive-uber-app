482be28d43542362f2ea174db69a245d9d6eace4
MelihYesilyurt <180202060@kocaeli.edu.tr>
2021-04-06 17:59:23 +0300

    Menü tasarımı tamamlandı

--- app/src/main/java/com/meliherdem/yazlab2_2/MainActivity.java
+++ app/src/main/java/com/meliherdem/yazlab2_2/MainActivity.java
@@ -1,84 +1,30 @@
 package com.meliherdem.yazlab2_2;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.meliherdem.yazlab2_2.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
-    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tabLayout= findViewById(R.id.tab_layout);
-        viewPager= findViewById(R.id.view_pager);
-
-        ArrayList<String> arrayList = new ArrayList<>();
-
-        arrayList.add("Tab 1");
-        arrayList.add("Tab 2");
-        arrayList.add("Tab 3");
-
-        prepareViewPager(viewPager,arrayList);
-        tabLayout.setupWithViewPager(viewPager);
-    }
-
-    private void prepareViewPager(ViewPager viewPager, ArrayList<String> arrayList) {
-        MainAdapter adapter = new MainAdapter(getSupportFragmentManager());
-        MainFragment fragment = new MainFragment();
-        for (int i=0; i<arrayList.size();i++)
-        {
-            Bundle bundle = new Bundle();
-            bundle.putString("title",arrayList.get(i));
-            fragment.setArguments(bundle);
-            adapter.addFragment(fragment,arrayList.get(i));
-            fragment= new MainFragment();
-        }
-        viewPager.setAdapter(adapter);
-    }
-    private class MainAdapter extends FragmentPagerAdapter {
-
-        ArrayList<String> arrayList = new ArrayList<>();
-        List<Fragment> fragmentList = new ArrayList<>();
-
-        public void addFragment(Fragment fragment,String title){
-            arrayList.add(title);
-            fragmentList.add(fragment);
-        }
-        public MainAdapter(@NonNull FragmentManager fm) {
-            super(fm);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-
-            return fragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return fragmentList.size();
-        }
-
-        @Nullable
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return arrayList.get(position);
-        }
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
     }
 }
