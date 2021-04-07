bdda326b9440f4f4cab59ace11109d2bb1a36a96
MelihYesilyurt <180202060@kocaeli.edu.tr>
2021-04-07 13:27:00 +0300

    Button aktif hale getirildi

--- app/src/main/java/com/meliherdem/yazlab2_2/Fragment2.java
+++ app/src/main/java/com/meliherdem/yazlab2_2/Fragment2.java
@@ -4,15 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
-    @Nullable
+    private Button sendBtn;
+    private TextView text;
+    private EditText textDate1;
+    private EditText textDate2;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment2_layout,container,false);
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.fragment2_layout,container,false);
+        sendBtn=view.findViewById(R.id.SendButton);
+        text= view.findViewById(R.id.WriteText);
+        textDate1= view.findViewById(R.id.TextDate1);
+        textDate2=view.findViewById(R.id.TextDate2);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("1.Tarih"+textDate1.getText()+"2.Tarih"+textDate2.getText());
+            }
+        });
+        return view;
     }
 }
