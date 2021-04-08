7a9cf1ef00b5ed42777f9c6526195e644318ef51
MelihYesilyurt <180202060@kocaeli.edu.tr>
2021-04-08 16:31:02 +0300

    3. için harita çizdirme eklendi

--- app/src/main/java/com/meliherdem/yazlab2_2/Fragment3.java
+++ app/src/main/java/com/meliherdem/yazlab2_2/Fragment3.java
@@ -1,9 +1,14 @@
 package com.meliherdem.yazlab2_2;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,32 +22,43 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Fragment3 extends Fragment {
+    String location1= "40.62572424,-73.82612577";
+    String location2= "42.62572424,-73.82612577";
+    Button btTrack;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment3_layout,container,false);
-
-        SupportMapFragment supportMapFragment=(SupportMapFragment)
-                getChildFragmentManager().findFragmentById(R.id.google_map);
-
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+        btTrack=view.findViewById(R.id.btTrack);
+        btTrack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(LatLng latLng) {
-                        MarkerOptions markerOptions = new MarkerOptions();
-                        markerOptions.position(latLng);
-                        markerOptions.title(latLng.latitude+ " : "+ latLng.longitude);
-                        googleMap.clear();
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                                latLng,10
-                        ));
-                        googleMap.addMarker(markerOptions);
-                    }
-                });
+            public void onClick(View v) {
+                String sSource = location1.trim();
+                String sDestination= location2.trim();
+
+                if(sSource.equals("")&&sDestination.equals(""))
+                {
+
+                }
+                else {
+                    DisplayTrack(sSource,sDestination);
+                }
             }
         });
-
         return view;
+    }
+
+    private void DisplayTrack(String sSource, String sDestination) {
+        try {
+            Uri uri = Uri.parse("https://www.google.co.in/maps/dir/"+sSource+"/"+sDestination);
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            intent.setPackage("com.google.android.apps.maps");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }catch (ActivityNotFoundException e){
+            Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps");
+            Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 }
